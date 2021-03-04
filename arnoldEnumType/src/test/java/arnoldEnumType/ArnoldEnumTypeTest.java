c56a07031c8ab89105700bb5e94eece4e8a78491
JoanRibot <jribot@cifpfbmoll.eu>
2021-03-04 17:43:28 +0100

    Pasando 4 caso test Alicia y Joan

--- arnoldEnumType/src/test/java/arnoldEnumType/ArnoldEnumTypeTest.java
+++ arnoldEnumType/src/test/java/arnoldEnumType/ArnoldEnumTypeTest.java
@@ -43,11 +43,11 @@ public class ArnoldEnumTypeTest {
 		assertThat(planeta.getMasa()).isEqualTo(3.303e+23);
 	}
 	
-	// @Test
-	// public void PlanetaGetRadioTest(){		
-	// 	Planeta planeta = Planeta.MERCURY;
-	// 	assertThat(planeta.getRadio()).isEqualTo(2.4397e+6);
-	// }	
+	@Test
+	public void PlanetaGetRadioTest(){		
+		Planeta planeta = Planeta.MERCURY;
+		assertThat(planeta.getRadio()).isEqualTo(2.4397e+6);
+	}	
 	
 	// @Test
 	// public void PlanetaNamesIteratorTest(){
